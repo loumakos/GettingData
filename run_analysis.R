b1e43bf24b7b2c7a643fef3ba098cf81075afcf8
loumakos <loumakos@utexas.edu>
2014-10-26 18:08:49 -0500

    Create run_analysis.R

---
+++ run_analysis.R
@@ -0,0 +1,9 @@
+#Loading the data
+
+#Here we merge the training and test sets
+
+#We provide names for the activities
+
+#We Label the data set with descriptive variable names
+
+#independent tidy data set with the average of each variable for each activity and each subject.
